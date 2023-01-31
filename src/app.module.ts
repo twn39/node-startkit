b1d554f872559dcf0737a3e5f3737ddb57c20b18
curry.tang <weinan.tang@consultant.LEGO.com>
2023-01-31 14:32:13 +0800

    fix: add undici and ioredis

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import Fastify, { FastifyInstance } from 'fastify';
 import { AppServer } from './app.server';
 import { HomeController } from './controllers/home.controller';
 import { Router } from './router';
+import { Redis } from 'ioredis';
+import { fetch } from 'undici';
 
 export const FASTIFY_INSTANCE = Symbol.for('fastify.instance');
+export const FETCH = Symbol.for('undici.fetch');
 
 const fastify: FastifyInstance = Fastify({ logger: true });
 
@@ -14,6 +17,16 @@ const fastify: FastifyInstance = Fastify({ logger: true });
     {
       provide: FASTIFY_INSTANCE,
       useValue: fastify,
+    },
+    {
+      provide: FETCH,
+      useValue: fetch,
+    },
+    {
+      provide: Redis,
+      useFactory: () => {
+        return new Redis();
+      },
     },
     HomeController,
     Router,
