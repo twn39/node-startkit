2cb2289a4763ec97ba4046bf56d23254abdd248d
curry.tang <weinan.tang@consultant.LEGO.com>
2023-03-06 17:28:36 +0800

    fix: update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   apps: [
     {
+      name: 'node-server',
       script: 'dist/main.js',
+      instances: 2,
+      exec_mode: 'cluster',
+      max_memory_restart: '150M',
+      env: {
+        PORT: 3040,
+        APP_ENV: 'prod',
+        NODE_ENV: 'production',
+      },
     },
   ],
 };
--- src/factories/fastifyFactory.ts
+++ src/factories/fastifyFactory.ts
@@ -6,6 +6,10 @@ export function fastifyFactory(config: IConfig) {
   const fastify: FastifyInstance = Fastify(config.server);
   fastify.register(helmet, {
     contentSecurityPolicy: false,
+    dnsPrefetchControl: false,
+    hsts: {
+      maxAge: 31536000,
+    },
   });
   return fastify;
 }
